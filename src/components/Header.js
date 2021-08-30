90148deb5480c4b4616e1c5f161b32e385dfb8e1
tiger-yash <tigeryashraj@gmail.com>
2021-08-30 23:13:59 +0530

    added card

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 import Logo from "./Logo";
 import { connect } from "react-redux";
 import { signOut, redirect, joinClass } from "../actions";
 import { Button } from "@material-ui/core";
 import SideNav from "./SideNav";
-// import JoinClassButton from './JoinClassButton'
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+import { green } from '@material-ui/core/colors';
+import { makeStyles } from '@material-ui/core/styles';
+import JoinClassCard from './JoinClassCard'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > span': {
+      margin: theme.spacing(2.5),
+    },
+  },
+}));
+
 
 const Header = ({ signOut, isSignedIn, redirect }) => {
+  const [showJoinDialog, setJoinDialog] = useState(false);
+  const confirm = () => {
+    setJoinDialog(false);
+  };
+  const cancel = () => {
+    setJoinDialog(false);
+  };
+
+  const classes = useStyles();
   const AuthButton = () => {
     let onClick = null,
       text = "Sign In";
@@ -18,17 +39,34 @@ const Header = ({ signOut, isSignedIn, redirect }) => {
       text = "Sign In";
     }
     return (
+      <>
+      <div className={classes.root}>
+      {/* <Button > */}
+        {/* <Icon style={{ color: green[500],fontSize: 30, marginTop:5  }}>add_circle</Icon> */}
+        <IconButton style={{ color: green[500]}} aria-label="add class" onClick={() => { setJoinDialog(true) }}>
+          <AddIcon />
+        </IconButton>
+        
+        {/* </Button> */}
+      </div>
       <Button variant="contained" color="primary" onClick={onClick}>
         <p className="">{text}</p>
       </Button>
+      </>
     );
   };
   return (
+    <>
     <div className="flex h-12 items-center p-5 sticky top-0 bg-gray-200">
       {isSignedIn ? <SideNav /> : null}
       <Logo className="mr-auto" />
       {AuthButton()}
     </div>
+    <JoinClassCard
+      show={showJoinDialog}
+      confirm={confirm}
+      cancel={cancel} />
+    </>
   );
 };
 
