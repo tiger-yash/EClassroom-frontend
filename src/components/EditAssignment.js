711a22296fdebb80dc0324aa6e96635cf5c3e097
adityachhaparia <adityachhaparia101@gmail.com>
2021-08-31 16:15:15 +0530

    Create EditTest, EditAssignment

---
+++ src/components/EditAssignment.js
@@ -0,0 +1,89 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { reduxForm, Field } from "redux-form";
+import { editAssignment, fetchAssignment } from "../actions";
+import RenderInput from "./RenderInput";
+import RenderDate from "./RenderDate";
+import Button from "@material-ui/core/Button";
+import useIsClassTeacher from "../hooks/useIsClassTeacher";
+
+const EditAssignment = props => {
+  const {
+    assignmentData,
+    classData,
+    editAssignment,
+    handleSubmit,
+    submitting,
+    change,
+    fetchAssignment
+  } = props;
+  const assignmentId = props.match.params.assignmentId;
+  useIsClassTeacher({ redirect: true });
+  useEffect(() => {
+    if (assignmentData) {
+      console.log(assignmentData);
+      change("assignment", assignmentData.assignmentName);
+      change("url", assignmentData.url);
+      change("max_marks", assignmentData.maxMarks);
+      change("due_date", assignmentData.dueDate);
+      change("end_date", assignmentData.endDate);
+    }
+  }, [change, assignmentData]);
+
+  useEffect(() => {
+    if (assignmentId && classData.classCode) {
+      console.log(assignmentId, classData.classCode);
+      fetchAssignment(assignmentId);
+    }
+  }, [classData.classCode, fetchAssignment, assignmentId]);
+
+  const submitHandler = values => {
+    editAssignment({ ...values, class_code: classData.classCode });
+  };
+  return (
+    <div className="w-1/2 mx-auto mt-4">
+      <h2 className="text-xl">Edit Assignment</h2>
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <Field name="assignment" type="text" component={RenderInput} label="Assignment Name" />
+        <Field name="url" type="text" component={RenderInput} label="Assignment Paper URL" />
+        <Field name="max_marks" type="number" component={RenderInput} label="Max Marks" />
+        <Field name="due_date" type="date" component={RenderDate} label="Due Date" />
+        <Field name="end_date" type="date" component={RenderDate} label="End Date" />
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          className="mr-3 mt-4"
+          disabled={submitting}>
+          <p className="">Submit</p>
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+const validate = values => {
+  const errors = {};
+  // TODO:
+  return errors;
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.user,
+    auth: state.auth,
+    classData: state.classes[ownProps.match.params.classId] ?? {},
+    assignmentData: state.assignments[ownProps.match.params.assignmentId] ?? {}
+  };
+};
+
+export default connect(mapStateToProps, { fetchAssignment, editAssignment })(
+  reduxForm({
+    form: "editAssignment",
+    validate
+    // initialValues: {
+    //   end_date: null,
+    //   due_date: null
+    // }
+  })(EditAssignment)
+);
---
+++ src/components/EditTest.js
@@ -0,0 +1,81 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { reduxForm, Field } from "redux-form";
+import { editTest, fetchTest } from "../actions";
+import RenderInput from "./RenderInput";
+import RenderDate from "./RenderDate";
+import Button from "@material-ui/core/Button";
+import useIsClassTeacher from "../hooks/useIsClassTeacher";
+
+const EditTest = props => {
+  const { testData, classData, editTest, handleSubmit, submitting, change, fetchTest } = props;
+  const testId = props.match.params.testId;
+  useIsClassTeacher({ redirect: true });
+  useEffect(() => {
+    if (testData) {
+      console.log(testData);
+      change("test", testData.testName);
+      change("url", testData.url);
+      change("max_marks", testData.maxMarks);
+      change("due_date", testData.dueDate);
+      change("end_date", testData.endDate);
+    }
+  }, [change, testData]);
+
+  useEffect(() => {
+    if (testId && classData.classCode) {
+      console.log(testId, classData.classCode);
+      fetchTest(testId);
+    }
+  }, [classData.classCode, fetchTest, testId]);
+
+  const submitHandler = values => {
+    editTest({ ...values, class_code: classData.classCode });
+  };
+  return (
+    <div className="w-1/2 mx-auto mt-4">
+      <h2 className="text-xl">Edit Test</h2>
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <Field name="test" type="text" component={RenderInput} label="Test Name" />
+        <Field name="url" type="text" component={RenderInput} label="Test Paper URL" />
+        <Field name="max_marks" type="number" component={RenderInput} label="Max Marks" />
+        <Field name="due_date" type="date" component={RenderDate} label="Due Date" />
+        <Field name="end_date" type="date" component={RenderDate} label="End Date" />
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          className="mr-3 mt-4"
+          disabled={submitting}>
+          <p className="">Submit</p>
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+const validate = values => {
+  const errors = {};
+  // TODO:
+  return errors;
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.user,
+    auth: state.auth,
+    classData: state.classes[ownProps.match.params.classId] ?? {},
+    testData: state.tests[ownProps.match.params.testId] ?? {}
+  };
+};
+
+export default connect(mapStateToProps, { fetchTest, editTest })(
+  reduxForm({
+    form: "editTest",
+    validate
+    // initialValues: {
+    //   end_date: null,
+    //   due_date: null
+    // }
+  })(EditTest)
+);
